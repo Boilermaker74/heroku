f977176ddddc90beb32912adc66177927a320a1d
Walter Moleterno <135560995+Boilermaker74@users.noreply.github.com>
2023-11-12 15:34:41 -0600

    Add files via upload

---
+++ routes/api/notes.js
@@ -0,0 +1,81 @@
+// Invoke npm packages
+
+const uuid = require('./uuid.js')
+const fs = require('fs')
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const express = require("express");
+const notes = express.Router();
+
+// Method for putting note on left side
+
+notes.get("/", (req, res) => {
+  readFile("./db/db.json")
+    .then((data) => {
+      const dbNotes = JSON.parse(data);
+      res.status(200).json(dbNotes);
+    })
+    .catch((err) => console.error(err));
+  console.info(`${req.method} request received to get all notes`);
+});
+
+notes.post("/", (req, res) => {
+  console.info(`${req.method} request received to add a task`);
+  const { title, text } = req.body;
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+      id: uuid(),
+    };
+
+    readFile("./db/db.json")
+      .then((data) => {
+        const dbNotes = JSON.parse(data);
+        dbNotes.push(newNote);
+        return dbNotes;
+      })
+      .then((dbNotes) => {
+        writeFile("./db/db.json", JSON.stringify(dbNotes, 0, 4));
+      })
+      .catch((err) => console.error(err));
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+    res.status(200).json(response);
+  } else {
+    res.status(404).send("Please supply both title and text!");
+  }
+});
+
+notes.delete("/:id", (req, res) => {
+  
+  console.info(`${req.method} request received to delete a note.`);
+  const { id } = req.params;
+  if (id) {
+    readFile("./db/db.json")
+      .then((data) => {
+        const dbNotes = JSON.parse(data);
+        const newDbNotes = dbNotes.filter((note) => {
+          return note.id !== id;
+        });
+        return newDbNotes;
+      })
+      .then((newDbNotes) => {
+        writeFile("./db/db.json", JSON.stringify(newDbNotes, 0, 4));
+      })
+      .catch((err) => console.error(err));
+    const response = {
+      status: "success",
+    };
+    res.status(200).json(response);
+  } else {
+    res.status(404).send(`The ID of ${id} is not found!`);
+  }
+});
+
+module.exports = notes;
+
+
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+//
+const router = require('express').Router();
+const notesRouter = require("./api/notes.js")
+router.use('/notes', notesRouter);
+module.exports = router;
